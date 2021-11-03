455dd5e146070f3eafa26984fb7be026d35d7a44
Neisanael <nathan123sembung@gmail.com>
2021-11-03 09:49:31 +0700

    Initial commit

---
+++ modul4/tugas1.py
@@ -0,0 +1,8 @@
+bilangan = int(input("Masukan bilangan: "))
+
+if bilangan % 2 == 0:
+    print ("%i adalah bilangan genap" %bilangan)
+else:
+    print ("%i adalah bilangan ganjil" %bilangan)
+
+# selesai
---
+++ modul4/tugas2.py
@@ -0,0 +1,9 @@
+namaa = input("Masukan nama : ")
+umura = int(input("Masukan umur : "))
+namab = input("Masukan nama : ")
+umurb = int(input("Masukan umur : "))
+
+if umura > umurb :
+    print ("\n{} lebih tua dari {}".format(namaa, namab))
+else :
+    print ("\n{} lebih muda dari {}".format(namaa, namab))
